ef6dcdefa91c462f0ccab89eaab8ffeba25f0579
Jacob Kato <jacobkato49@yahoo.com>
2017-07-16 00:43:04 -0700

    added npm modules and finished keys.js

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log("twitterKeys have been loaded");
+
+
+//Please enter your twitter keys here so the LIRI-Bot can recall your past tweets
+/**If you don't have a twitter or would not like LIRI-Bot to
+recall your past tweets leave the twitterKeys blank**/
+exports.twitterKeys ={
+  consumer_key: "",
+  consumer_secret: "",
+  access_token_key: "",
+  access_token_secret: ""
+}
